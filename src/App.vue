ae889a6a29069e4728a17e009ac59e1af70172de
xtwenty <dan@xtwenty.com>
2019-04-14 21:27:22 +0200

    [add] all the things

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,106 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" v-bind:style="stylesObj">
+        <SalesPitch :msg="msg"/>
+        <span class="instructions">press space</span>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import SalesPitch from './components/SalesPitch.vue';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            SalesPitch
+        },
+        data() {
+            return {
+                msg: 'DREAMS',
+                msgs: ['DREAMS', 'DRUGS'],
+                textColor: '',
+                bgColor: '',
+                hasListener: false
+            }
+        },
+        methods: {
+            handleKeydown(e) {
+                if (e.keyCode === 32) {
+                    this.updateColor();
+                    this.updateText();
+                }
+            },
+            updateColor() {
+                const rHue = r(0, 360);
+                const rSat = r(60, 80);
+                const rLig = r(40, 60);
+                const cHue = rHue - 180 >= 0 ? rHue - 180 : rHue + 180;
+                this.textColor = `hsl(${cHue},${rSat}%,${rLig}%)`;
+                this.bgColor = `hsl(${rHue},${rSat}%,${rLig}%)`;
+            },
+            updateText(){
+                if (this.msgs.length === 0) this.fetchWords();
+                const newMsg = this.msgs[r(0, this.msgs.length)];
+                this.msg = newMsg.word.toUpperCase();
+            },
+            async fetchWords() {
+                const response = await fetch(`https://api.datamuse.com/words?sp=d*s&topics=mundane,technology,food&md=p&max=1000`);
+                const data = await response.json();
+                const nouns = data.filter(item => item.tags && item.tags.includes("n"));
+                this.msgs = [...this.msgs,...nouns];
+            }
+        },
+        mounted() {
+            this.fetchWords();
+            this.updateColor();
+            !this.hasListener && window.addEventListener('keydown', this.handleKeydown);
+            this.hasListener = true;
+        },
+        computed: {
+            stylesObj: function() {
+                return {
+                    'color': this.textColor,
+                    'backgroundColor': this.bgColor
+                }
+            }
+        }
+    }
+
+    function r(min, max) {
+        if (max === null) {
+            max = min;
+            min = 0;
+        }
+        return Math.floor(Math.random() * ( max - min + 1) + min);
+    }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @import url('https://fonts.googleapis.com/css?family=Work+Sans:700,800&IBM+Plex+Mono');
+
+    body {
+        min-height: 100vh;
+        background: gray;
+        margin: 0;
+    }
+
+    .instructions {
+        position: absolute;
+        bottom: 1em;
+        margin-top: auto;
+        font-weight: normal;
+        font-family: 'IBM Plex Mono', monospace;
+    }
+
+    #app {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: white;
+        height: 100vh;
+        transition: background-color 0.5s ease, color 1s ease;
+        font-family: "Work Sans", sans-serif;
+        font-weight: 700;
+    }
 </style>
---
+++ src/components/SalesPitch.vue
@@ -0,0 +1,70 @@
+
+<template>
+    <div class="salesPitch">
+        <h1 ref="title">
+            <span class="line">We are selling</span>
+            <span class="line">{{ msg }},</span>
+            <span class="line">not calls to action</span>
+        </h1>
+    </div>
+</template>
+
+<script>
+    /* eslint-disable */
+    import { TweenMax, Power2 } from 'gsap';
+
+    export default {
+        name: 'SalesPitch',
+        props: {
+            msg: {
+                type: String,
+                default: 'DREAMS'
+            }
+        },
+        methods: {
+            fadeInUp() {
+               const lines = [...this.$refs.title.querySelectorAll('span')];
+               TweenMax.set(lines, {y: 100, opacity: 0});
+               TweenMax.staggerTo(lines, 0.5, {opacity: 1, ease: Power2.easeOut }, 0.075);
+               TweenMax.staggerTo(lines, 1, {y: 0, opacity: 1, ease: Power2.easeOut}, 0.075);
+            }
+        },
+        watch: {
+            msg: function() {
+                this.fadeInUp();
+            }
+        },
+        mounted() {
+            this.fadeInUp();
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+    h1 {
+        font-size: 3em;
+        font-weight: 800;
+        margin: 0;
+        text-align: center;
+        word-wrap: break-word;
+        hyphens: auto;
+    }
+
+
+    @media (min-width: 768px) {
+        h1 {
+            font-size: 4.5em;
+        }
+    }
+    @media (min-width: 1000px) {
+        h1 {
+            font-size: 6em;
+        }
+    }
+
+    span.line {
+        display: block;
+    }
+</style>
